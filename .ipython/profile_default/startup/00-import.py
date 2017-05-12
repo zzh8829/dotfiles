59ba4047676886861e33b8973ba26d3bc75a52b6
Zihao Zhang <zzh8829@gmail.com>
2017-05-12 02:20:38 -0400

    Python

---
+++ .ipython/profile_default/startup/00-import.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import math
+import random
+import time
+import pickle
+import json
+import requests
+import numpy as np
+import pandas as pd
+import scipy as sp
+import seaborn as sns
+import matplotlib.pyplot as plt
+import sympy
---
+++ python.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+# Removed user's cached credentials
+# This script might be run with .dots, which uses elevated privileges
+sudo -K
+
+###############################################################################
+# Virtual Enviroments                                                         #
+###############################################################################
+
+echo "------------------------------"
+echo "Setting up virtual environments."
+
+pip3 install virtualenv
+pip3 install virtualenvwrapper
+
+###############################################################################
+# Python 2 Virtual Enviroment                                                 #
+###############################################################################
+
+# echo "------------------------------"
+# echo "Setting up py2-data virtual environment."
+
+# # Create a Python2 data environment
+# mkvirtualenv --python=$(which python2) py2
+# workon py2
+
+# # Install Python data modules
+# pip install numpy
+# pip install scipy
+# pip install matplotlib
+# pip install pandas
+# pip install sympy
+# pip install nose
+# pip install unittest2
+# pip install seaborn
+# pip install scikit-learn
+# pip install "ipython[all]"
+# pip install bokeh
+# pip install Flask
+# pip install sqlalchemy
+# pip install mysql-python
+
+###############################################################################
+# Python 3 Virtual Enviroment                                                 #
+###############################################################################
+
+echo "------------------------------"
+echo "Setting up py3 virtual environment."
+
+# Create a Python3 data environment
+mkvirtualenv --python=$(which python3) py3
+workon py3
+
+# Install Python data modules
+pip install bokeh
+pip install ipython
+pip install matplotlib
+pip install numpy
+pip install pandas
+pip install scikit-learn
+pip install scipy
+pip install seaborn
+pip install sympy
+
+pip install jupyter
+
+pip install tensorflow
+pip install keras
+
+pip install requests
+pip install bs4
+pip install Flask
+
+echo "------------------------------"
+echo "Script completed."
+# echo "Usage: workon py2 for Python2"
+echo "Usage: workon py3 for Python3"
