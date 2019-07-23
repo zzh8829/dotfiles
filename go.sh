99c98bd8271621079b8ffb32841ee73560d2ade0
Zihao Zhang <zzh8829@gmail.com>
2019-07-23 14:43:44 -0700

    Fix go

--- go.sh
+++ go.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-if [[ $OS == 'linux' || $OS == 'wsl']]; then
+if [[ $OS == 'linux' || $OS == 'wsl' ]]; then
     sudo apt-get install go
-elif [[ $OS == 'macos']]; then
+elif [[ $OS == 'macos' ]]; then
     brew install go
 else
     echo 'Unknonw OS skipping go install'
