45b35d0b17bc1848e453416cce849f4b94146bb1
Zihao Zhang <zzh8829@gmail.com>
2019-08-29 13:44:29 -0400

    Gnu stow is great

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 
-rsync --archive --verbose \
-	--exclude ".git/" \
-	--exclude ".gitkeep" \
-	--exclude ".DS_Store" \
-	--include ".*" \
-	--include ".*/**" \
-	--exclude '*' \
-	"$@" \
-	. ~/
+# rsync --archive --verbose \
+# 	--exclude ".git/" \
+# 	--exclude ".gitkeep" \
+# 	--exclude ".DS_Store" \
+# 	--include ".*" \
+# 	--include ".*/**" \
+# 	--exclude '*' \
+# 	"$@" \
+# 	. ~/
+
+stow --adopt --verbose \
+	bin \
+	conda \
+	editorconfig \
+	eslint \
+	gcloud \
+	git \
+	ipython \
+	matplotlib \
+	pylint \
+	tmux \
+	vim \
+	zsh \
