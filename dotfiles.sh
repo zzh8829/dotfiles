5247a553ae7585cd6c507c13f6687d197596fdd0
Zihao Zhang <zzh8829@gmail.com>
2019-09-06 23:53:06 -0700

    Add conflict resolution

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/env bash
 
-# rsync --archive --verbose \
-# 	--exclude ".git/" \
-# 	--exclude ".gitkeep" \
-# 	--exclude ".DS_Store" \
-# 	--include ".*" \
-# 	--include ".*/**" \
-# 	--exclude '*' \
-# 	"$@" \
-# 	. ~/
+PACKAGES=(
+	bin
+	conda
+	editorconfig
+	eslint
+	gcloud
+	git
+	ipython
+	matplotlib
+	pylint
+	tmux
+	vim
+	zsh
+)
 
-stow --no-folding --verbose \
-	bin \
-	conda \
-	editorconfig \
-	eslint \
-	gcloud \
-	git \
-	ipython \
-	matplotlib \
-	pylint \
-	tmux \
-	vim \
-	zsh \
+for PKG in ${PACKAGES[@]}; do
+	CONFLICTS=$(stow --no --verbose $PKG 2>&1 | awk '/\* existing target is/ {print $NF}')
+	for filename in ${CONFLICTS[@]}; do
+		if [[ -f $HOME/$filename || -L $HOME/$filename ]]; then
+			echo "DELETE: $filename"
+			rm -f "$HOME/$filename"
+		fi
+	done
+
+	stow --no-folding --verbose $PKG
+done
