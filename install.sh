d3d309ab236b8d492e74b7953527da0b6ec7da8e
Zihao Zhang <zzh8829@gmail.com>
2019-12-11 17:38:24 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,19 @@
 
 set -ex
 
+if type git &> /dev/null; then
+  echo "Git Found"
+if type apt-get &> /dev/null; then
+	sudo apt-get update
+	sudo apt-get install -yyq git
+elif type yum &> /dev/null; then
+	sudo yum update
+	sudo yum install git
+else
+	echo "Could not find or install git"
+	exit 1
+fi
+
 cd ~
 rm -rf ~/.dotfiles
 git clone git@github.com:zzh8829/dotfiles .dotfiles
