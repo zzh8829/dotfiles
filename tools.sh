66836fdfed870fafe4fc25f5e8031c925905643e
Zihao Zhang <zzh8829@gmail.com>
2024-11-15 14:54:38 -0800

    python

--- tools.sh
+++ tools.sh
@@ -1,3 +1,4 @@
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
 asdf plugin-add kubectl https://github.com/asdf-community/asdf-kubectl.git
+asdf plugin-add python
